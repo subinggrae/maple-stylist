42b8e11d08c3c41bad74171b764bb0228791f9f2
subinggrae <subin000602@gmail.com>
2025-03-12 17:42:41 +0900

    feat: 헤어 염색 기능 추가

--- src/item-scrapper.py
+++ src/item-scrapper.py
@@ -5,12 +5,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
+driver.maximize_window()
 wait = WebDriverWait(driver, 10)
 
 
 def init_mannequin():
-  init_button = driver.find_element(By.XPATH, '//button[text()="프리셋 초기화"]')
-  init_button.click()
+  init_btn = driver.find_element(By.XPATH, '//button[text()="프리셋 초기화"]')
+  init_btn.click()
 
   wait.until(
     EC.presence_of_element_located((By.XPATH, '//button[text()="초기화"]'))
@@ -38,11 +39,30 @@ def choose_beauty_item(hair, face):
   search_box.send_keys(Keys.BACK_SPACE)
 
 
+def dye_hair(base_color, mix_color=None, mix_ratio=0):
+  dye_mode_btn = driver.find_element(By.XPATH, '//button[text()="염색모드"]')
+  dye_mode_btn.click()
+
+  base_color_img = wait.until(
+    EC.presence_of_element_located((By.XPATH, f'//img[@alt="{base_color}"]'))
+  )
+  base_color_img.click()
+
+  if mix_color:
+    mix_color_img = driver.find_elements(By.XPATH, f'//img[@alt="{mix_color}"]')[1]
+    driver.execute_script('arguments[0].scrollIntoView(true)', mix_color_img)
+    mix_color_img.click()
+    mix_ratio_input = driver.find_elements(By.XPATH, '//input[@inputmode="numeric"]')[1]
+    mix_ratio_input.send_keys(Keys.CONTROL + 'a')
+    mix_ratio_input.send_keys(mix_ratio)
+
+
 if __name__ == '__main__':
   driver.get('https://meaegi.com/dressing-room')
 
   init_mannequin()
   choose_beauty_item('데이즈 헤어(여)', '조용한 눈 얼굴(여)')
+  dye_hair('초', '갈', 59)
 
   input('아무거나 입력하세요.')
 
