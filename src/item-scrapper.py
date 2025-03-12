8d89d5df5d52b106e24cf68f60ea1e2e94709249
subinggrae <subin000602@gmail.com>
2025-03-12 16:01:29 +0900

    fix: 마네킹 초기화 기능

--- src/item-scrapper.py
+++ src/item-scrapper.py
@@ -1,7 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 10)
 
-driver.get('https://meaegi.com/dressing-room')
 
-driver.quit()
+def init_mannequin():
+  init_button = driver.find_element(By.XPATH, '//button[text()="프리셋 초기화"]')
+  init_button.click()
+
+  wait.until(
+    EC.presence_of_element_located((By.XPATH, '//button[text()="초기화"]'))
+  ).click()
+
+
+if __name__ == '__main__':
+  driver.get('https://meaegi.com/dressing-room')
+
+  init_mannequin()
+
+  driver.quit()
